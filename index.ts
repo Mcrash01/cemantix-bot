4d966865f855e826e1c593ccdcdb97f5949430ac
Mcrash01 <90929317+Mcrash01@users.noreply.github.com>
2024-10-22 14:54:29 +0200

    update api

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ async function handler(_req: Request): Promise<Response> {
 
   const url: string = _req.url;
   const guess: string = url.split("/")[ -1];
-  alert(guess);
+  console.log(url, guess);
 
   const similarityRequestBody = JSON.stringify({
     word1: "centrale",
